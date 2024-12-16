0f46b4e1e02d43c3db0d19d4e069987a0ffb8546
DevanshGupta15 <devanshguptahero@gmail.com>
2024-12-16 23:37:41 +0530

    sa

--- backend/index.js
+++ backend/index.js
@@ -20,26 +20,9 @@ const app = express();
 //   allowedHeaders: ['Content-Type', 'Authorization']
 // }));
 
-// Dynamically set allowed origins
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:4200',
-  'http://localhost:10000',
-  'https://concalls-trans-3.onrender.com'
-];
-
-// CORS configuration
 app.use(cors({
-  origin: function(origin, callback){
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  // origin: process.env.CORS_ORIGIN,
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
