2d486fcfe41ac135f8973502d56a1b6af275e66d
Devansh <55797810+DevanshGupta15@users.noreply.github.com>
2024-12-16 23:35:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,28 +21,12 @@ const app = express();
 // }));
 
 // Dynamically set allowed origins
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:4200',
-  'http://localhost:10000',
-  'https://concalls-trans-3.onrender.com'
-];
-
-// CORS configuration
 app.use(cors({
-  origin: function(origin, callback){
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    // origin: process.env.CORS_ORIGIN,
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 
 app.use(express.json());
